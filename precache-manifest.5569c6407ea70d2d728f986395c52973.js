043d50571a080fa68f550ca383f43961a743a148
Yasir Sarole <yasirsarole81@gmail.com>
2019-12-25 03:17:22 +0530

    Updates

---
+++ precache-manifest.5569c6407ea70d2d728f986395c52973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071de88f0d1c64717ea708de3b42b186",
+    "url": "/ui--assignment/index.html"
+  },
+  {
+    "revision": "58926ce2537f561739ce",
+    "url": "/ui--assignment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "12fde364d63ca4d26580",
+    "url": "/ui--assignment/static/js/2.c5f5b4ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ui--assignment/static/js/2.c5f5b4ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58926ce2537f561739ce",
+    "url": "/ui--assignment/static/js/main.1b9da3f6.chunk.js"
+  },
+  {
+    "revision": "f9901da7f3db846152d8",
+    "url": "/ui--assignment/static/js/runtime-main.84298676.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ui--assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
