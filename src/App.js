ed348509e645d76525fca7cecc9eaf509210773f
Yasir Sarole <yasirsarole81@gmail.com>
2019-12-25 13:23:06 +0530

    added styled components and added required icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
 import eventsData from "./data";
+
+// import icons
+import {
+  MdDashboard,
+  MdEvent,
+  MdSettings,
+  MdKeyboardArrowDown,
+  MdFormatListBulleted,
+  MdEdit
+} from "react-icons/md";
+import { FaStar, FaUsers, FaBell, FaRegCalendar } from "react-icons/fa";
+import { TiNews } from "react-icons/ti";
+import { GiScrewdriver } from "react-icons/gi";
+import { GoSignIn } from "react-icons/go";
+import { FiPlus, FiSearch, FiGrid } from "react-icons/fi";
 
 class App extends React.Component {
   componentDidMount() {
@@ -26,33 +42,41 @@ class App extends React.Component {
       <>
         <header>
           <div className="wrapper">
-            <span class="community">
+            <span className="community">
               community<span>manager</span>
             </span>
             <nav>
               <ul>
                 <li>
+                  <MdDashboard />
                   <a href="#Fixme">dashboard</a>
                 </li>
                 <li>
+                  <MdEvent />
                   <a href="#Fixme">events</a>
                 </li>
                 <li>
+                  <FaStar />
                   <a href="#Fixme">facilities</a>
                 </li>
                 <li>
+                  <TiNews />
                   <a href="#Fixme">news</a>
                 </li>
                 <li>
+                  <GiScrewdriver />
                   <a href="#Fixme">maintenance</a>
                 </li>
                 <li>
+                  <GoSignIn />
                   <a href="#Fixme">sign ins</a>
                 </li>
                 <li>
+                  <FaUsers />
                   <a href="#Fixme">users</a>
                 </li>
                 <li>
+                  <MdSettings />
                   <a href="#Fixme">settings</a>
                 </li>
               </ul>
